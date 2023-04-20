f7c98b6ff6b77d0f22e3d0d8e5c2e47a88c205a1
Arash <47273556+arashnrim@users.noreply.github.com>
2023-04-20 02:22:10 +0000

    Complete questions

--- Question2.java
+++ Question2.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
-public class Question2
-{
-  public static void main(String[] args)
-  {
+public class Question2 {
+  public static void main(String[] args) {
     /**
-     * Prompts user for height and weight, calculate and print out the BMI of the user
+     * Prompts user for height and weight, calculate and print out the BMI of the
+     * user
      * 
      * Hint: in.nextDouble() to read double
      */
-     
+
     Scanner in = new Scanner(System.in);
     double height = in.nextDouble();
+    double weight = in.nextDouble();
+    in.close();
+
+    double bmi = weight / (height * height);
+    System.out.println(bmi);
   }
 }
--- Question4.java
+++ Question4.java
@@ -1,23 +1,30 @@
 import java.util.Scanner;
 
-public class Question4
-{
-  public static void main(String[] args)
-  {
+public class Question4 {
+  public static void main(String[] args) {
     /**
-     * Prints a right angle triangle with *. The base of the triangle depends on the integer given.
-     *    e.g.
-     *     > 5
-     *     *****
-     *     ****
-     *     ***
-     *     **
-     *     *
+     * Prints a right angle triangle with *. The base of the triangle depends on the
+     * integer given.
+     * e.g.
+     * > 5
+     * *****
+     * ****
+     * ***
+     * **
+     * *
      * 
      * Hint: 2 loops are required. System.out.print(...) to print on single line.
      */
-     
+
     Scanner in = new Scanner(System.in);
-    
+    int height = in.nextInt();
+    in.close();
+
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < height - i; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
   }
 }
